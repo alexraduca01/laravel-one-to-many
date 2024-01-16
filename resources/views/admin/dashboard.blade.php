bd5b96e856ca93db6e2b64ea19ac730f3091b274
alexraduca01 <alex.raduca01@icloud.com>
2024-01-16 15:06:23 +0100

    edited dashboard style

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -17,35 +17,7 @@
                     </div>
                     @endif
                     <div>
-                        <table class="table table-dark table-striped">
-                            <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Name</th>
-                                    <th>Description</th>
-                                    <th>Git</th>
-                                    <th>Settings</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($projects as $item)
-                                    <tr>
-                                        <td>#{{ $item->id }}</td>
-                                        <td><a class="text-white text-decoration-none " href="{{ route('admin.projects.show', $item->slug) }}">{{ $item->title }}</a></td>
-                                        <td>{{ $item->body }}</td>
-                                        <td>{{ $item->url }}</td>
-                                        <td>
-                                            <a href="{{ route('admin.projects.edit', $item->slug) }}" class="btn btn-success"><i class="fa-solid fa-pen"></i></a>
-                                            <form action="{{route('admin.projects.destroy', $item->slug)}}" method="POST" class="d-inline">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="delete-button btn btn-danger" data-item-title="{{ $item->title }}"><i class="fa-solid fa-trash-can"></i></button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+                        {{ __('You are logged in!') }}
                     </div>
                 </div>
             </div>
